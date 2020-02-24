fa04b84f23940ff94ff91aa535e57ed70cc55367
Ilona Khafizova <ilona.khafizova@workato.com>
2020-02-23 21:36:39 -0800

    added router, update mocks

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,38 @@
 import React, {PureComponent} from "react";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import MoviePage from "../movie-page/movie-page.jsx";
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
   }
 
-  render() {
+  _renderApp() {
     const {films} = this.props;
 
     return (
       <Main
         films={films}
       />
+    );
+  }
+
+  render() {
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+
+          </Route>
+          <Route exact path="/dev-film">
+            <MoviePage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,41 +2,57 @@ export default [
   {
     name: `Fantastic Beasts: The Crimes of Grindelwald`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `fantastic-beasts-the-crimes-of-grindelwald.jpg`
   },
   {
     name: `Bohemian Rhapsody`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `bohemian-rhapsody.jpg`
   },
   {
     name: `Macbeth`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `macbeth.jpg`
   },
   {
     name: `Aviator`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `aviator.jpg`
   },
   {
     name: `We need to talk about Kevin`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `we-need-to-talk-about-kevin.jpg`
   },
   {
     name: `What We Do in the Shadows`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `what-we-do-in-the-shadows.jpg`
   },
   {
     name: `revenant.jpg`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `Revenant`
   },
   {
     name: `johnny-english.jpg`,
     genre: `comedy`,
+    year: `1999`,
+    poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
     preview: `Johnny English`
   }
 ];
